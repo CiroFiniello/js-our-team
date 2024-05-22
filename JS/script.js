fa8844464f3f4b205b39c9e6e41342c47f33f876
CodeKiro <kirofiniello@outlook.it>
2024-05-23 00:31:42 +0200

    the array have been readed from the for, and writed on console -milestone 1

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,40 @@
 const team = 
 [
 {
-    'nome' : 'wayne Barnett',
-    'ruolo' : 'Founder & Ceo',
-    'foto' : 'wayne-barnett-founder-ceo.jpg',   
+    nome: 'wayne Barnett',
+    ruolo: 'Founder & Ceo',
+    foto: 'wayne-barnett-founder-ceo.jpg',   
 },
 {
-    'nome' : 'Angela Caroll',
-    'ruolo' : 'Chief Editor',
-    'foto' : 'angela-caroll-chief-editor.jpg',
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    foto: 'angela-caroll-chief-editor.jpg',
 },
 {
-    'nome' : 'Walter Gordon',
-    'ruolo' : 'Office Manager',
-    'foto' : 'walter-gordon-office-manager.jpg',
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    foto: 'walter-gordon-office-manager.jpg',
 },
 {
-    'nome' : 'Angela Lopez',
-    'ruolo' : 'Social Media Manager',
-    'foto' : 'angela-lopez-social-media-manager.jpg',
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    foto: 'angela-lopez-social-media-manager.jpg',
 },
 {
-    'nome' : 'Scott Estrada',
-    'ruolo' : 'Developer',
-    'foto' : 'scott-estrada-developer.jpg',
+    nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    foto: 'scott-estrada-developer.jpg',
 },
 {
-    'nome' : 'Barbara Ramos',
-    'ruolo' : 'Graphic Designer',
-    'foto' : 'barbara-ramos-graphic-designer.jpg',
+    nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    foto: 'barbara-ramos-graphic-designer.jpg',
 },
 ];
+
+// stampa in pagina 
+for (let index = 0; index < team.length; index++) {
+    console.log(team[index]);   
+}
+
+
